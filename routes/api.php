1ef3e86a87a43b905e7b63751011af85c94e96f2
gerson <gerson.barros@m2n.com.br>
2020-06-05 01:24:21 -0300

    criação da rota da controller ProductController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('products', 'api\ProductController');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
