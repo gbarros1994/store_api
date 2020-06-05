ac6a16aac4ae8cd4f84fae8682f44b0f69c054e4
gerson <gerson.barros@m2n.com.br>
2020-06-05 01:52:05 -0300

    limpeza de funções não utilizadas

--- app/Http/Controllers/api/PurchaseController.php
+++ app/Http/Controllers/api/PurchaseController.php
@@ -10,16 +10,6 @@ use App\Models\Purchase;
 class PurchaseController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-       
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -120,39 +110,5 @@ class PurchaseController extends Controller
 
         curl_close($curl);
         return $response;
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
