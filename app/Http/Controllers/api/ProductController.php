b6b04ed56168ea639915ab79e8711dfb02711960
gerson <gerson.barros@m2n.com.br>
2020-06-05 01:50:35 -0300

    criação da função para deltar o produto

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -38,15 +38,6 @@ class ProductController extends Controller
         }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -135,17 +126,6 @@ class ProductController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -165,6 +145,25 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            $product = Product::findOrFail($id);
+
+            if($product->delete()) {
+                return response()->json(
+                    ['message' => 'Produto excluído com sucesso'],
+                    200
+                );
+            } else {
+                return response()->json(
+                    ['message' => 'Erro nos dados enviados'],
+                    400
+                );
+            }
+        } catch (\Throwable $th) {
+            return response()->json(
+                    ['message' => 'Erro interno no servidor'],
+                    500
+                );
+        }
     }
 }
