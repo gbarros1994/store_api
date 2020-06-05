3cbaa346d2498c072b8b1d3236a53745dbfb7e53
gerson <gerson.barros@m2n.com.br>
2020-06-05 01:49:48 -0300

    Criação da função de compra e confi da rota de compra

--- app/Http/Controllers/api/PurchaseController.php
+++ app/Http/Controllers/api/PurchaseController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Purchase;
 
 class PurchaseController extends Controller
 {
@@ -14,17 +16,7 @@ class PurchaseController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+       
     }
 
     /**
@@ -34,8 +26,100 @@ class PurchaseController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
+    {     
+        try {
+            $product = Product::find($request['product_id']);
+
+            //VERIFICA SE EXISTE PRODUTO
+            if ($product) {
+                //VERIFICA SE HA EM ESTOQUE
+                if ($request['quantity_purchased'] <= $product['qty_stock']) { 
+                    $content = array (  
+                        'amount' => $product['amount'] * $request['quantity_purchased'],
+                        'card'   => array (
+                            'owner'           => isset($request->card['owner']) ? $request->card['owner'] : '',
+                            'card_number'     => isset($request->card['card_number']) ? $request->card['card_number'] : '',
+                            'date_expiration' => isset($request->card['date_expiration']) ? $request->card['date_expiration'] : '',
+                            'brand'           => isset($request->card['brand']) ? $request->card['brand'] : '',
+                            'cvv'             => isset($request->card['cvv']) ? $request->card['cvv'] : '',
+                            'flag'            => isset($request->card['flag']) ? $request->card['flag'] : '',
+                        )
+                    );
+            
+                    $response = $this->shopping(json_encode($content), "POST");
+                    //VERIFICA SE HOUVE RESPOSTA
+                    if (json_decode($response)->status == 200) {
+                        $purchase = Purchase::create([
+                            'product_id'         => $request['product_id'],
+                            'quantity_purchased' => $product['amount'] * $request['quantity_purchased'],
+                            'owner'              => $request->card['owner'],
+                            'card_number'        => $request->card['card_number'],
+                            'date_expiration'    => $request->card['date_expiration'],
+                            'brand'              => $request->card['brand'],
+                            'cvv'                => $request->card['cvv'],
+                            'flag'               => $request->card['flag'],
+                        ]);
+
+                        //VERIFICA SE FOI CRIADO NA TABELA ANTES DE DAR O UPDATE NO ESTOQUE
+                        if ($purchase) {
+                            Product::where('id', $request['product_id'])->update(['qty_stock' => $product['qty_stock'] - $request['quantity_purchased']]);
+                            return $response;
+                        } else {
+                            return response()->json([
+                                'status' => 400, 
+                                'message' => 'Erro nos dados enviados'
+                            ]);
+                        }
+                    } else {
+                        return $response;
+                    }
+                } else {
+                    return response()->json([
+                        'status' => 400, 
+                        'message' => 'Não há estoque suficiente'
+                    ]);
+                }
+            } else {
+                return response()->json([
+                    'status' => 400, 
+                    'message' => 'Não há estoque suficiente'
+                ]);
+            }
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => 500, 
+                'message' => 'Erro interno no servidor'
+            ]);
+        }
+        
+    }
+
+    public function shopping($content, $type)
     {
-        //
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.devhp.com.br/test/gateway/shopping",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $type,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_POSTFIELDS => $content,
+            CURLOPT_HTTPHEADER => array(
+                "x-auth-token: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9bOjoxXVwvZHVnYV9hcGlcLyIsImF1ZCI6Imh0dHA6XC9cL1s6OjFdXC9kdWdhX2FwaVwvIiwiaWF0IjoxNTg3NzM2Njk2LCJzdWIiOiIzMTE0In0.z4mEO3Jeem9KH0_En2RwnJekUmIUOc3t0aIUzrV1uO0",
+                "Content-Type: application/json"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
     }
 
     /**
@@ -45,17 +129,6 @@ class PurchaseController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
     {
         //
     }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('products', 'api\ProductController');
+Route::apiResource('purchase', 'api\PurchaseController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
